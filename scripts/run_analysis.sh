a765eef2ca97d2f3690361f05ce94c736fc62e25
atharv <atharv55215@gmail.com>
2025-10-01 22:04:06 +0530

    Using new docker container

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 if [ -z "$REPO_URL" ]; then
@@ -9,15 +8,16 @@ fi
 
 REPO_NAME=$(basename -s .git "$REPO_URL")
 CLONE_DIR="./temp/$REPO_NAME"
-REPORT_FILE="./temp_reports/report_${REPO_NAME}.json"
+REPORT_DIR="./temp_reports"
+REPORT_FILE="$REPORT_DIR/report_${REPO_NAME}.xml"
 
-mkdir -p temp_reports
+mkdir -p "$REPORT_DIR"
 
-echo "Running Checkstyle in Docker for $REPO_NAME..."
-
+# Run Checkstyle in a temporary container and automatically remove it
 docker run --rm \
-  -v "$(pwd)/$CLONE_DIR:/workspace" \
-  ghcr.io/checkstyle/checkstyle \
-  -c /google_checks.xml -f json /workspace > "$REPORT_FILE" || true
+  -v "$(pwd)/$CLONE_DIR:/$REPO_NAME" \
+  --workdir "/$REPO_NAME" \
+  atharv3321/checkstyle:latest \
+  -c /google_checks.xml -f xml "/$REPO_NAME" > "$REPORT_FILE" || true
 
 echo "Analysis complete. Report saved to $REPORT_FILE"
